e716569347df4e212d32b546e9e6e0a706fd2f04
urassaya <vanessafoxandrade@hotmail.com>
2024-03-20 00:18:46 +0100

    update

--- conditionals/travelcost.py
+++ conditionals/travelcost.py
@@ -2,7 +2,7 @@ km = float(input('How far are you traveling in kilometres? '))
 
 if km <= 200:
     cost = km * 0.50
-    print(f'The cost is {cost:.2f}€')
 else:
     cost = km * 0.45
-    print(f'The cost is {cost:.2f}€')
+
+print(f'The cost is {cost:.2f}€')
