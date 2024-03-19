e56e92c6452c3b9ed2781ec5c6e312603a7706b7
urassaya <vanessafoxandrade@hotmail.com>
2024-03-19 23:25:10 +0100

    commit

--- conditionals/guess.py
+++ conditionals/guess.py
@@ -1,8 +1,18 @@
 from random import randint
-num = randint(1, 9)
-guess = int(input('Guess the number I chose: '))
+from time import sleep
+
+num = randint(1, 9)  # um número aleatório de 1 a 9
+print('-=-' * 20)
+guess = int(
+    input("Guess the number I'm thinking of, a number beetween 1 and 9: "))
+print('-=-' * 20)
+
+print('Loading...')
+sleep(3)  # faz o computador esperar 3 segundos antes de executar o próximo comando
 
 if guess == num:
-    print('You guessed it!')
+    print(f'You guessed it! It is {guess}.')
 else:
-    print('It is not right...')
+    print(f'It is not right... I thought {num} and not {guess}.')
+
+print()
