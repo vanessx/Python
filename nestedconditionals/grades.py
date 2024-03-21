eb2e8edc321e902b5ce39dac1554b59bc491d932
urassaya <vanessafoxandrade@hotmail.com>
2024-03-21 20:14:45 +0100

    commit

---
+++ nestedconditionals/grades.py
@@ -0,0 +1,11 @@
+x = float(input('First grade (1-10): '))
+y = float(input('Second grade (1-10): '))
+
+average = (x + y) / 2
+
+if average < 5:
+    print('\033[31mRejected!\033[m')
+elif average >= 5 and average <= 6.9:
+    print('\033[33mSummer school!\033[m')
+else:
+    print('\033[32mApproved!\033[m')
