c99441006aa1ee45ac3a651a81b72c3da979e2a2
urassaya <vanessafoxandrade@hotmail.com>
2024-03-21 21:04:28 +0100

    update

--- nestedconditionals/triangle.py
+++ nestedconditionals/triangle.py
@@ -2,12 +2,16 @@ print('-=-' * 6)
 print('Triangle Analysis')
 print('-=-' * 6)
 
+colors = {'close': '\033[m',
+        'red': '\033[31m',
+        'green': '\033[32m'}
+
 a = int(input('First line: '))
 b = int(input('Second line: '))
 c = int(input('Third line: '))
 
 if a < b + c and b < a + c and c < a + b:
-    print('You can form a triangle.')
+    print(colors['green'],'You can form a triangle.', colors['close'])
     if a == b and b == c:
         print('Is an equilateral triangle.')
     elif a == b or b == c or a == c:
@@ -15,4 +19,4 @@ if a < b + c and b < a + c and c < a + b:
     else:
         print('Is a scalene triangle.')
 else:
-    print('You can not form a triangle.')
+    print(colors['red'],'You can not form a triangle.', colors['close'])
