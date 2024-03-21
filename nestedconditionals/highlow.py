4b51a39280f6f4f5a20d29cdc4afbbaab5586968
urassaya <vanessafoxandrade@hotmail.com>
2024-03-21 19:12:10 +0100

    commit

---
+++ nestedconditionals/highlow.py
@@ -0,0 +1,9 @@
+n1 = int(input('First number: '))
+n2 = int(input('Second number: '))
+
+if n1 > n2:
+    print(f'{n1} is greater than {n2}')
+elif n1 < n2:
+    print(f'{n1} is less than {n2}')
+else:
+    print(f'{n1} is equal to {n2}')
