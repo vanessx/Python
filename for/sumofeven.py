8f9e210e9b4c0f456e33a844ccb1c8be066929ec
urassaya <vanessafoxandrade@hotmail.com>
2024-03-23 23:19:43 +0100

    update

--- for/sumofeven.py
+++ for/sumofeven.py
@@ -1,8 +1,10 @@
 sum = 0
+count = 0
 
 for c in range(1, 7):
     num = int(input('Number: '))
     if num % 2 == 0:
         sum += num
+        count += 1
 
-print(f'The sum of the even numbers is \033[31m{sum}\033[m.')
+print(f'The sum of {count} even numbers is \033[31m{sum}\033[m.')
