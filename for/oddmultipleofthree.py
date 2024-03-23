2d4df2abfe8eecb0dcc5836b1ec5f49dd2419f99
urassaya <vanessafoxandrade@hotmail.com>
2024-03-23 21:49:27 +0100

    update

--- for/oddmultipleofthree.py
+++ for/oddmultipleofthree.py
@@ -1,7 +1,9 @@
-sum = 0
+sum = 0 # acumulador
+count = 0 # contador
 
-for c in range (1, 500):
+for c in range(1, 501, 2):
     if c % 3 == 0:
+        count += 1
         sum += c
 
-print(c)
+print(f'The sum of all \033[33m{count}\033[m values is \033[31m{sum}\033[m.')
