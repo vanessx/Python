7d368be7c09dc95aa277774347e891ebc900a522
urassaya <vanessafoxandrade@hotmail.com>
2024-03-24 23:09:41 +0100

    Update majoritygroup.py

--- for/majoritygroup.py
+++ for/majoritygroup.py
@@ -1,12 +1,15 @@
 from datetime import date
 
 year = date.today().year
-count = 0
+count_over = 0
+count_under = 0
 
 for c in range(1, 7):
     birth = int(input('Year of birth: '))
     age = year - birth
     if age >= 18:
-        count += 1
+        count_over += 1
+    else:
+        count_under += 1
 
-print(f'{count} people over the age of majority.\n{6 - count} people under the age of majority.')
+print(f'{count_over} people over the age of majority.\n{count_under} people under the age of majority.')
