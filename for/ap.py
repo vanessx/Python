ee4c8e8d16f631151813d6da8c8de9c190c0fec9
urassaya <vanessafoxandrade@hotmail.com>
2024-03-24 21:04:26 +0100

    Create ap.py

---
+++ for/ap.py
@@ -0,0 +1,6 @@
+term = int(input('First term: '))
+ratio = int(input('Ratio: '))
+tenth = term + (10 - 1) * ratio # eu quero que mostre os primeiros 10 termos, se eu quisesse 15 colocava 15 e por aí...
+
+for c in range(term, tenth, ratio):
+    print(c, end=' ')
