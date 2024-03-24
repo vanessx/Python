5e4a7423597a0319b72ae78fd74ef1f946058ded
urassaya <vanessafoxandrade@hotmail.com>
2024-03-24 21:35:09 +0100

    Update primenumber.py

--- for/primenumber.py
+++ for/primenumber.py
@@ -5,7 +5,7 @@ for c in range(1, num + 1):
     if num % c == 0:
         total += 1 # vai acumular quantas vezes o número é divisivel
 
-if total <= 2:
+if total == 2: # só é primo se for divisivel por 1 e por ele mesmo
     print(f'The number {num} is a prime number.')
 else:
     print(f'The number {num} is not a prime number.')
