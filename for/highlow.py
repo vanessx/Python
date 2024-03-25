7b27e847ec6dc951dca537532f9e6d1736f6f4df
urassaya <vanessafoxandrade@hotmail.com>
2024-03-25 20:12:26 +0100

    Create highlow.py

---
+++ for/highlow.py
@@ -0,0 +1,15 @@
+higher = 0
+lower = 0
+
+for w in range(1, 6):
+    weight = float(input(f'What is the weight of the {w}° person: '))
+    if w == 1:
+        higher = weight
+        lower = weight
+    else:
+        if weight > higher:
+            higher = weight
+        if weight < lower:
+            lower = weight
+
+print(f'The highest weight is {higher:.1f}kg and the lowest is {lower:.1f}kg.')
