8b71ee0c895638082452f836119a480f1644b1e6
urassaya <vanessafoxandrade@hotmail.com>
2024-04-11 23:31:12 +0200

    Create football_teams.py

---
+++ tuples/football_teams.py
@@ -0,0 +1,21 @@
+teams = ('sporting', 'benfica', 'porto', 'braga', 'vitória SC', 'moreirense',
+         'arouca', 'famalicão', 'casa pia', 'farense', 'boavista', 'rio ave',
+         'estoril', 'gil vicente', 'estrela amadora', 'portimonense', 'vizela',
+         'chaves')
+
+print('\033[4mThe top5 is:\033[m')
+for team in teams[0:5]:
+    print(team.title())
+
+print('\n\033[4mThe last 4 are:\033[m')
+for team in teams[-4:]:
+    print(team.title())
+
+print('\n\033[4mThe teams in alphabetical order:\033[m')
+for team in sorted(teams):
+    print(team.title())
+
+for pos, team in enumerate(teams):
+    if team == 'porto':
+        print(f'\n{team.title()} is in position {pos + 1}.')
+    
