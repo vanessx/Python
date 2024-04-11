caf4d9b8370f519e68d53369dfe6724d294acc7d
urassaya <vanessafoxandrade@hotmail.com>
2024-04-11 23:57:59 +0200

    Create greater_less_values.py

---
+++ tuples/greater_less_values.py
@@ -0,0 +1,18 @@
+from random import randint
+
+numbers = (randint(1,10), randint(1,10), randint(1,10), randint(1,10), randint(1,10))
+
+print(f'This is the list: {numbers}')
+
+less = 0
+greater = 0
+for number in numbers:
+    if number == numbers[0]:
+        less = number
+        greater = number
+    elif number > greater:
+        greater = number
+    elif number < less:
+        less = number
+
+print(f'{greater} is the greater and {less} is the less one.')
