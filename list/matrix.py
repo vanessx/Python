e1b09c6ac8ce01a04dc843889bc63abef2ae061e
vanessa <51798590+vanessx@users.noreply.github.com>
2024-09-10 21:11:14 +0200

    commit

--- list/matrix.py
+++ list/matrix.py
@@ -2,7 +2,7 @@ matrix = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 total = sumcol = 0
 
 for l in range(0, 3):
-    for c in range (0, 3): # linha zero coluna 1, 2, 3 -> linha 1 coluna 1, 2, 3...
+    for c in range (0, 3):
         matrix[l][c] = int(input('Type a number: '))
         if matrix[l][c] % 2 == 0:
             total += matrix[l][c]
@@ -13,7 +13,7 @@ for l in range(0, 3):
     print()
 
 for l in range(0, 3):
-    sumcol += matrix[l][2] # na linha zero pega o valor na posiçao 2, na linha um pega o valor na posiçao 2...
+    sumcol += matrix[l][2] 
 
 print(f'The sum of even numbers is: {total}\n'
       f'The sum of the numbers in the third column is: {sumcol}\n'
